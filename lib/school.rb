f8678aad8e1592ec26d2645e316fb40158b9c820
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-25 00:50:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,4 +6,15 @@ class School
     @roster = {}
   end
 
+  def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
+
+  def grade(student_grade)
+    roster[student_grade]
+  end
+    
+  end
+
 end
